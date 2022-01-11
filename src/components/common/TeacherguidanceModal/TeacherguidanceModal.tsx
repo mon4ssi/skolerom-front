a0111ce218e3fccffada48992d0fa1ca33d64039
Marco Utrilla <marco.utrilla@itware.com.pe>
2022-01-11 17:06:09 -0500

    TEM-2246 - advances

--- src/components/common/TeacherguidanceModal/TeacherguidanceModal.tsx
+++ src/components/common/TeacherguidanceModal/TeacherguidanceModal.tsx
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
 import intl from 'react-intl-universal';
 import { DescriptionEditor } from 'assignment/view/NewAssignment/Questions/DescriptionEditor';
-import { DraftTeachingPath } from 'teachingPath/teachingPathDraft/TeachingPathDraft';
+import { DraftTeachingPath, EditableTeachingPathNode } from 'teachingPath/teachingPathDraft/TeachingPathDraft';
 import { ItemContentTypeContext } from 'teachingPath/view/EditTeachingPath/ItemContentTypeContext';
 import './TeacherguidanceModal.scss';
 import { CreateButton } from '../CreateButton/CreateButton';
-import trashImg from 'assets/images/trash-tp.svg';
+import closeImg from 'assets/images/modal-close.svg';
 
 interface Props {
   currentEntity: DraftTeachingPath;
+}
+
+interface Teacherguidance {
+  nroLevel: number;
+  nroLetter: number;
+  hideBorderTop: boolean;
+  children: EditableTeachingPathNode;
 }
 
 @inject('editTeachingPathStore')
@@ -31,7 +38,88 @@ export class TeacherguidanceModal extends Component<Props> {
     modalTGBack[0].classList.add('hide');
   }
 
+  public renderFirstNode = () => {
+    const { currentEntity } = this.props;
+
+    if (currentEntity.content.children.length > 0) {
+      return (
+        <div className="modalContentTG__body__row">
+          <h4>
+            <div className="nestedOrderNumber">1</div>
+            {currentEntity.content.selectQuestion}
+          </h4>
+          <DescriptionEditor
+            description={currentEntity.content.guidance}
+            onChange={(value: string) => { currentEntity.content.setGuidance(value); }}
+          />
+        </div>
+      );
+    }
+  }
+  public renderChildrenNode = () => {
+    const { currentEntity } = this.props;
+    let nroLevelLoop: number = 2;
+    const firstLetterNumber: number = 97;
+
+    const childrenFinal: Array<Teacherguidance> = [];
+    let children: Array<EditableTeachingPathNode> = [];
+    let childrenTmp: Array<EditableTeachingPathNode> = currentEntity.content.children;
+    let nroNodes: number = 0;
+    let nroLetterLoop: number = firstLetterNumber;
+    let hideBorderTopLoop: boolean = true;
+    let continueLoop = true;
+
+    while (continueLoop) {
+      nroNodes = 0;
+      children = childrenTmp;
+      childrenTmp = [];
+
+      children.forEach((item) => {
+        if (item.children.length > 0) {
+          item.children.forEach((child) => {
+            if (child.children.length > 0) {
+              childrenTmp.push(child);
+            }
+          });
+
+          const child:Teacherguidance = {
+            nroLevel: nroLevelLoop,
+            nroLetter: nroLetterLoop,
+            hideBorderTop: hideBorderTopLoop,
+            children: item
+          };
+
+          childrenFinal.push(child);
+          nroLetterLoop += 1;
+          nroNodes += 1;
+          hideBorderTopLoop = false;
+        }
+      });
+
+      nroLevelLoop += 1;
+      nroLetterLoop = firstLetterNumber;
+      hideBorderTopLoop = true;
+      continueLoop = (nroNodes > 0);
+    }
+
+    return childrenFinal.map((item, index) => (
+            <div className={item.hideBorderTop ? 'modalContentTG__body__row line' : 'modalContentTG__body__row'} key={index}>
+              <h4>
+                <div className="nestedOrderNumber">{item.nroLevel}</div>
+                {item.children.selectQuestion} (option {String.fromCharCode(item.nroLetter)})
+              </h4>
+              <DescriptionEditor
+                description={item.children.guidance}
+                onChange={(value: string) => { item.children.setGuidance(value); }}
+              />
+            </div>
+          )
+        );
+  }
+
   public render() {
+    const { currentEntity } = this.props;
+
     return (
       <div>
         <div className="btnTeacherguide">
@@ -51,7 +139,7 @@ export class TeacherguidanceModal extends Component<Props> {
             <span>How is the teacher supposed to use this teaching path?</span>
             <div className="modalContentTG__header__close">
               <img
-                src={trashImg}
+                src={closeImg}
                 alt={intl.get('generals.close')}
                 title={intl.get('generals.close')}
                 onClick={this.closeModalTG}
@@ -59,7 +147,14 @@ export class TeacherguidanceModal extends Component<Props> {
             </div>
           </div>
           <div className="modalContentTG__body">
-            123456
+            <div className="modalContentTG__body__row">
+              <DescriptionEditor
+                description={currentEntity.guidance}
+                onChange={(value: string) => { currentEntity.setGuidance(value); }}
+              />
+            </div>
+            {this.renderFirstNode()}
+            {this.renderChildrenNode()}
           </div>
           <div className="modalContentTG__footer">
             <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
