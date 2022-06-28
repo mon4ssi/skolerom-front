9f03a5d473df0d5b5957225295b8abcd9d147a55
DESKTOP-5ISCA9B\Usuario <frank.delarosa@itware.com.pe>
2022-06-28 11:09:00 -0500

    TEM-2380 #time fixeds 3h

--- src/assignment/view/tabs/MyAssignments/MyAssignments.tsx
+++ src/assignment/view/tabs/MyAssignments/MyAssignments.tsx
@@ -39,7 +39,7 @@ class MyAssignments extends Component<Props> {
 
   private async fetchAssignments() {
     const { filter } = this.props.assignmentListStore!;
-
+    const myFilterSchool = (this.props.typeOfAssignmentsList === 'myschool') ? 1 : 0;
     filter.page = QueryStringHelper.getNumber(this.props.history, QueryStringKeys.PAGE, 1);
     filter.grade = QueryStringHelper.getNumber(this.props.history, QueryStringKeys.GRADE);
     filter.subject = QueryStringHelper.getNumber(this.props.history, QueryStringKeys.SUBJECT);
@@ -52,9 +52,7 @@ class MyAssignments extends Component<Props> {
     filter.order = QueryStringHelper.getString(this.props.history, QueryStringKeys.ORDER, SortingFilter.DESC);
     filter.articles = QueryStringHelper.getString(this.props.history, QueryStringKeys.ARTICLES);
     filter.orderField = SortingFilter.CREATION_DATE;
-    if (this.props.typeOfAssignmentsList === 'myschool') {
-      filter.showMySchoolTeachingpath = QueryStringHelper.getNumber(this.props.history, QueryStringKeys.MYSCHOOL, 1);
-    }
+    filter.showMySchoolTeachingpath = QueryStringHelper.getNumber(this.props.history, QueryStringKeys.MYSCHOOL, myFilterSchool);
     if (filter.subject || filter.grade || filter.grepCoreElementsIds || filter.grepMainTopicsIds || filter.grepGoalsIds || filter.grepReadingInSubject || filter.source) {
       this.setState({ filtersisUsed: true });
     } else {
