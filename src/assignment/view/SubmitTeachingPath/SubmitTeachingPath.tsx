0465313d2dea2c63ba10794d805a881a59b7c5c0
FrankDevDeLaRosa <frank.delarosa@itware.com.pe>
2022-08-03 15:58:59 -0500

    TEM-2420 #time 4h fixeds submits

--- src/assignment/view/SubmitTeachingPath/SubmitTeachingPath.tsx
+++ src/assignment/view/SubmitTeachingPath/SubmitTeachingPath.tsx
@@ -16,31 +16,33 @@ export class SubmitTeachingPath extends Component<Props> {
     const button = (this.props.isPreview) ? intl.get('teaching path passing.complete.button_preview') : intl.get('teaching path passing.complete.button');
     return (
       <div className="SubmitTeachingPath">
-        <div className="SubmitTeachingPath__title">
-          {title}
-        </div>
+        <div className="SubmitTeachingPath__content">
+          <div className="SubmitTeachingPath__title">
+            {title}
+          </div>
 
-        <div className="SubmitTeachingPath__description">
-          {description}
-        </div>
+          <div className="SubmitTeachingPath__description">
+            {description}
+          </div>
 
-        {/*<button className="SubmitTeachingPath__deleteButton" onClick={this.props.onDelete}>*/}
-        {/*  {intl.get('teaching path passing.complete.delete_all_answers')}*/}
-        {/*</button>*/}
+          {/*<button className="SubmitTeachingPath__deleteButton" onClick={this.props.onDelete}>*/}
+          {/*  {intl.get('teaching path passing.complete.delete_all_answers')}*/}
+          {/*</button>*/}
 
-        <button
-          className="SubmitTeachingPath__button"
-          onClick={this.props.onSubmit}
-          title={intl.get('current_assignment_page.submit')}
-        >
-          <img
-            className="SubmitTeachingPath__buttonImage"
-            src={submitAnswersIcon}
-            alt={intl.get('current_assignment_page.submit')}
+          <button
+            className="SubmitTeachingPath__button"
+            onClick={this.props.onSubmit}
             title={intl.get('current_assignment_page.submit')}
-          />
-          {button}
-        </button>
+          >
+            <img
+              className="SubmitTeachingPath__buttonImage"
+              src={submitAnswersIcon}
+              alt={intl.get('current_assignment_page.submit')}
+              title={intl.get('current_assignment_page.submit')}
+            />
+            {button}
+          </button>
+        </div>
       </div>
     );
   }
