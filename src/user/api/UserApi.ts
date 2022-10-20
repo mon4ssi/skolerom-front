fdcbf1d0eda87ce30e2f26b71443e3c0782f6282
DESKTOP-5ISCA9B\Usuario <frank.delarosa@itware.com.pe>
2022-10-20 11:16:31 -0500

    test

--- src/user/api/UserApi.ts
+++ src/user/api/UserApi.ts
@@ -45,7 +45,7 @@ export class UserApi implements UserRepo {
       if (error.response.status === STATUS_NOT_FOUND || error.response.status === STATUS_SERVER_ERROR) {
         Notification.create({
           type: NotificationTypes.ERROR,
-          title: String(error.response)
+          title: String(error.response.data)
         });
         return null;
       }
