dc9a4a2dd4f1a6cf93c29f34b845c96c795434e5
FrankDevDeLaRosa <frank.delarosa@itware.com.pe>
2023-03-09 11:50:42 -0500

    Fixeds article list

--- src/teachingPath/view/EditTeachingPath/AddItemModal/ArticlesList/ArticlesList.tsx
+++ src/teachingPath/view/EditTeachingPath/AddItemModal/ArticlesList/ArticlesList.tsx
@@ -1874,14 +1874,8 @@ export class ArticlesList extends Component<Props, State> {
       return <div className={'noResults'}>{intl.get('edit_teaching_path.No results found')}</div>;
     }
 
-    return articlesForSkeletonEight.map((skeletonArticle, index) => (
-      <SkeletonLoader
-        key={index}
-        className="RelatedArticlesCard"
-        width={skeletonCardWidth}
-        height={skeletonCardHeight}
-      />
-    )
+    return (
+      selectedAndLoadedArticles.map(this.renderArticle)
     );
   }
 
