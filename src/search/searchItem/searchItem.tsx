55862efab0f4f1ed4a5fc2fd639cd454ba12b811
FrankDevDeLaRosa <frank.delarosa@itware.com.pe>
2023-06-30 16:44:41 -0500

    TEM-2737 #time 4h fixeds

--- src/search/searchItem/searchItem.tsx
+++ src/search/searchItem/searchItem.tsx
@@ -2,16 +2,19 @@ import React, { Component, SyntheticEvent } from 'react';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 import intl from 'react-intl-universal';
 import { inject, observer } from 'mobx-react';
+import { injector } from 'Injector';
 import debounce from 'lodash/debounce';
 import { SearchStore } from '../SearchStore';
 import { Search, SimpleNumberData } from '../Search';
 import { DEBOUNCE_TIME } from 'utils/constants';
 import { lettersNoEn } from 'utils/lettersNoEn';
+import { ActionMenu } from 'search/searchItem/searchItemModal/searchItemModal';
 import { SortingFilter, StoreState, QueryStringKeys } from 'utils/enums';
 import { UserType } from 'user/User';
 import * as QueryStringHelper from 'utils/QueryStringHelper';
 import { TeachingPathsListStore } from '../../teachingPath/view/TeachingPathsList/TeachingPathsListStore';
 import { EditTeachingPathStore } from '../../teachingPath/view/EditTeachingPath/EditTeachingPathStore';
+import { AssignmentService, ASSIGNMENT_SERVICE } from 'assignment/service';
 
 import more from 'assets/images/hovered-more.svg';
 import './searchItem.scss';
@@ -26,13 +29,16 @@ interface SearchProps {
 
 interface SearchState {
   stateTP: boolean;
+  stateAssignment: boolean;
 }
 
 @inject('searchStore', 'teachingPathsListStore', 'editTeachingPathStore')
 @observer
 class SearchItem extends Component<SearchProps & RouteComponentProps, SearchState> {
+  private assignmentService: AssignmentService = injector.get(ASSIGNMENT_SERVICE);
   public state = {
-    stateTP: false
+    stateTP: false,
+    stateAssignment: false
   };
 
   public renderItemSubject = (item: string) => (
@@ -69,6 +75,29 @@ class SearchItem extends Component<SearchProps & RouteComponentProps, SearchStat
     }
   }
 
+  public changeStateAssignment = () => {
+    if (this.state.stateAssignment) {
+      this.setState({
+        stateAssignment: false
+      });
+    } else {
+      this.setState({
+        stateAssignment: true
+      });
+    }
+  }
+
+  public onClose = () => {
+    this.setState({
+      stateTP: false
+    });
+  }
+
+  public onCloseAssignment = () => {
+    this.setState({
+      stateAssignment: false
+    });
+  }
   public editTeachingPath = (id: number) => {
     const { history } = this.props;
     history.push(`/teaching-paths/edit/${id}`);
@@ -106,17 +135,91 @@ class SearchItem extends Component<SearchProps & RouteComponentProps, SearchStat
     history.push(`/teaching-paths/view/${id}`);
   }
 
+  public MyeditTeachingPath = () => {
+    const { history, item } = this.props;
+    const id = item.id;
+    history.push(`/teaching-paths/edit/${id}`);
+  }
+
+  public MyeditAssignment = () => {
+    const { history, item } = this.props;
+    const id = item.id;
+    history.push(`/assignments/edit/${id}`);
+  }
+
+  public MyviewTeachingPath = () => {
+    const { history, item } = this.props;
+    const id = item.id;
+    history.push(`/teaching-paths/view/${id}`);
+  }
+
+  public MycopyTeachingPath = async () => {
+    const { teachingPathsListStore, history, item } = this.props;
+    const currentUserType = teachingPathsListStore!.getCurrentUser()!.type;
+    const id = item.id;
+    if (currentUserType === UserType.Teacher || currentUserType === UserType.ContentManager) {
+      const copyId = await teachingPathsListStore!.copyEntity(id);
+      history.push(`/teaching-paths/edit/${copyId}`);
+    }
+    history.push(`/teaching-paths/view/${id}`);
+  }
+
+  public MycopyAssignment = async () => {
+    const { teachingPathsListStore, history, item } = this.props;
+    const currentUserType = teachingPathsListStore!.getCurrentUser()!.type;
+    const id = item.id;
+    if (currentUserType === UserType.Teacher || currentUserType === UserType.ContentManager) {
+      /* const currentEntityRoute = entityStore instanceof AssignmentListStore ? 'assignments' : 'teaching-paths'; */
+      const copyId = await this.assignmentService.copyAssignment(id!);
+      history.push(`/assignments/edit/${copyId}`);
+    }
+  }
+
+  public MydeleteTeachingPath = async () => {
+    const { teachingPathsListStore, editTeachingPathStore, item } = this.props;
+    const currentUserType = teachingPathsListStore!.getCurrentUser()!.type;
+    const id = item.id;
+    if (currentUserType === UserType.Teacher || currentUserType === UserType.ContentManager) {
+      await editTeachingPathStore!.deleteTeachingPathById(id);
+      window.location.reload();
+    }
+  }
+
+  public MydeleteAssignment = async () => {
+    const { teachingPathsListStore, editTeachingPathStore, item } = this.props;
+    const currentUserType = teachingPathsListStore!.getCurrentUser()!.type;
+    const id = item.id;
+    if (currentUserType === UserType.Teacher || currentUserType === UserType.ContentManager) {
+      await this.assignmentService.removeAssignment(id);
+      window.location.reload();
+    }
+  }
+
   public renderTPOptions = () => {
     const { id } = this.props.item;
     return (
-      <div className="cardInfoItem__tooltip">
-        <ul className="flexBox dirColumn">
-          <li className={'fw500 flexBox fs15 editOrDeleteValue'}><a href="javascript:void(0)" onClick={() => { this.editTeachingPath(id); }}>{intl.get('teaching_paths_list.edit')}</a></li>
-          <li className={'fw500 flexBox fs15 editOrDeleteValue'}><a href="javascript:void(0)" onClick={() => { this.viewTeachingPath(id); }}>{intl.get('teaching_paths_list.view')}</a></li>
-          <li className={'fw500 flexBox fs15 editOrDeleteValue'}><a href="javascript:void(0)" onClick={() => { this.copyTeachingPath(id); }}>{intl.get('teaching_paths_list.copy')}</a></li>
-          <li className={'fw500 flexBox fs15 editOrDeleteValue'}><a href="javascript:void(0)" onClick={() => { this.deleteTeachingPath(id); }}>{intl.get('teaching_paths_list.delete')}</a></li>
-        </ul>
-      </div>
+      <ActionMenu
+        editTeachingPath={this.MyeditTeachingPath}
+        viewTeachingPath={this.MyviewTeachingPath!}
+        copyTeachingPath={this.MycopyTeachingPath}
+        deleteTeachingPath={this.MydeleteTeachingPath}
+        id={id}
+        onClose={this.onClose}
+      />
+    );
+  }
+
+  public renderAssignmentOptions = () => {
+    const { id } = this.props.item;
+    return (
+      <ActionMenu
+        editTeachingPath={this.MyeditAssignment}
+        copyTeachingPath={this.MycopyAssignment}
+        deleteTeachingPath={this.MydeleteAssignment}
+        id={id}
+        onClose={this.onCloseAssignment}
+        assignment
+      />
     );
   }
 
@@ -126,6 +229,18 @@ class SearchItem extends Component<SearchProps & RouteComponentProps, SearchStat
         <img src={more} />
       </button>
     </div>
+  )
+
+  public renderMoreAssigment = () => (
+    <div className="more-show" onClick={this.changeStateAssignment}>
+      <button>
+        <img src={more} />
+      </button>
+    </div>
+  )
+
+  public asideContent = () => (
+    <div>aqui va contenido de wordpress OMG</div>
   )
 
   public render() {
@@ -142,12 +257,17 @@ class SearchItem extends Component<SearchProps & RouteComponentProps, SearchStat
       case 'ARTICLE':
         return (
           <div className="cardInfoItem">
-            <div className="cardInfoItem__imagen">
-              <img src={featuredImg} />
+            <div className="cardInfoItem__card">
+              <div className="cardInfoItem__imagen">
+                <img src={featuredImg} />
+              </div>
+              <div className="cardInfoItem__description">
+                <div className="title"><h2>{title}</h2></div>
+                <div className="description">{description}</div>
+              </div>
             </div>
-            <div className="cardInfoItem__description">
-              <div className="title"><h2>{title}</h2></div>
-              <div className="description">{description}</div>
+            <div className="cardInfoItem__detail">
+              {this.asideContent()}
             </div>
           </div>
         );
@@ -176,11 +296,8 @@ class SearchItem extends Component<SearchProps & RouteComponentProps, SearchStat
             </div>
             <div className="cardInfoItem__right">
               {this.renderSubject(subjects)}
-              <div className="more-show">
-                <button>
-                  <img src={more} />
-                </button>
-              </div>
+              {visibility && this.renderMoreAssigment()}
+              {visibility && this.state.stateAssignment && this.renderAssignmentOptions()}
             </div>
           </div>
         );
--- src/search/searchItem/searchItemModal/searchItemModal.tsx
+++ src/search/searchItem/searchItemModal/searchItemModal.tsx
@@ -6,25 +6,39 @@ import onClickOutside from 'react-onclickoutside';
 
 interface ActionMenuProps {
   id: number;
+  assignment?: boolean;
   onClose: () => void;
-  editTeachingPath: (id: number) => void;
-  viewTeachingPath: (id: number) => void;
-  copyTeachingPath: (id: number) => void;
-  deleteTeachingPath: (id: number) => void;
+  editTeachingPath: () => void;
+  viewTeachingPath?: () => void;
+  copyTeachingPath: () => void;
+  deleteTeachingPath: () => void;
 }
 
 class ActionMenu extends Component<ActionMenuProps> {
+  public handleClickOutside = () => this.props.onClose();
   public render() {
+    if (this.props.assignment) {
+      return (
+        <div className="cardInfoItem__tooltip">
+          <ul className="flexBox dirColumn">
+          <li className={'fw500 flexBox fs15 editOrDeleteValue'}><a href="javascript:void(0)" onClick={() => { this.props.editTeachingPath(); }}>{intl.get('assignment list.Edit assignment')}</a></li>
+          <li className={'fw500 flexBox fs15 editOrDeleteValue'}><a href="javascript:void(0)" onClick={() => { this.props.copyTeachingPath(); }}>{intl.get('assignment list.Copy assignment')}</a></li>
+          <li className={'fw500 flexBox fs15 editOrDeleteValue'}><a href="javascript:void(0)" onClick={() => { this.props.deleteTeachingPath(); }}>{intl.get('assignment list.Delete assignment')}</a></li>
+          </ul>
+        </div>
+      );
+    }
     return (
       <div className="cardInfoItem__tooltip">
         <ul className="flexBox dirColumn">
-        <li className={'fw500 flexBox fs15 editOrDeleteValue'}><a href="javascript:void(0)" onClick={() => { this.props.editTeachingPath(this.props.id); }}>{intl.get('teaching_paths_list.edit')}</a></li>
-        <li className={'fw500 flexBox fs15 editOrDeleteValue'}><a href="javascript:void(0)" onClick={() => { this.props.viewTeachingPath(this.props.id); }}>{intl.get('teaching_paths_list.view')}</a></li>
-        <li className={'fw500 flexBox fs15 editOrDeleteValue'}><a href="javascript:void(0)" onClick={() => { this.props.copyTeachingPath(this.props.id); }}>{intl.get('teaching_paths_list.copy')}</a></li>
-        <li className={'fw500 flexBox fs15 editOrDeleteValue'}><a href="javascript:void(0)" onClick={() => { this.props.deleteTeachingPath(this.props.id); }}>{intl.get('teaching_paths_list.delete')}</a></li>
+        <li className={'fw500 flexBox fs15 editOrDeleteValue'}><a href="javascript:void(0)" onClick={() => { this.props.editTeachingPath(); }}>{intl.get('teaching_paths_list.edit')}</a></li>
+        <li className={'fw500 flexBox fs15 editOrDeleteValue'}><a href="javascript:void(0)" onClick={() => { this.props.viewTeachingPath!(); }}>{intl.get('teaching_paths_list.view')}</a></li>
+        <li className={'fw500 flexBox fs15 editOrDeleteValue'}><a href="javascript:void(0)" onClick={() => { this.props.copyTeachingPath(); }}>{intl.get('teaching_paths_list.copy')}</a></li>
+        <li className={'fw500 flexBox fs15 editOrDeleteValue'}><a href="javascript:void(0)" onClick={() => { this.props.deleteTeachingPath(); }}>{intl.get('teaching_paths_list.delete')}</a></li>
         </ul>
       </div>
     );
   }
 }
 const ActionMenuComponent = onClickOutside(ActionMenu);
+export { ActionMenuComponent as ActionMenu };
