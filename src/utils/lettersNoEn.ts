25f777ef1389ed4e335bcc108b8a2a57461a92ea
Frank De La Rosa <frankdelarosacampos@gmail.com>
2024-05-22 08:15:03 -0500

    merge letters

--- src/utils/lettersNoEn.ts
+++ src/utils/lettersNoEn.ts
@@ -1,4 +1,4 @@
-export const lettersNoEn = (string: string): boolean => {  
+export const lettersNoEn = (string: string): boolean => {
   const regExp = /^[0-9a-zA-Z,.?!_()!?:‘;#%{}@$^&*=+"'’«»“—”–<>/\-[\] æøåáàÆØÅâéèêóôòöœōõäãāÈIOUÜÄËÖÜäëöü]*$/;
   return regExp.test(string);
 };
