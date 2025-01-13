ac21a8b269cf7fe8e8fd1ea29ea19980a6391b40
Monassi Carrasco <monassi.carrasco@dbtarget.pe>
2025-01-13 11:23:37 -0500

    feat: add check if env is local

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,11 +1,17 @@
 import { local } from './WebStorage';
 
+const ENV = process.env.REACT_APP_ENV as string;
 const IS_DEVELOPMENT = 'IS_DEVELOPMENT';
+
+/**
+ * Checks if the current environment is local.
+ */
+const isLocal = () => ENV === 'local';
 
 /**
  * Checks if the current environment is development.
  */
-const isDevelopment = () => local.boolean(IS_DEVELOPMENT);
+const isDevelopment = () => isLocal() || local.boolean(IS_DEVELOPMENT);
 
 export const storage = {
   local: {
